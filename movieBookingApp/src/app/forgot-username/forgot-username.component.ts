c00879b2685eea33d996894d510ab96846eddb3c
Sushil Rajeeva Bhandary <sushilrajeeva@gmail.com>
2023-06-19 02:40:39 +0530

    Added popup support to display success and error message for forgot username component
    Added popup support to display success and error message for forgot username component

--- movieBookingApp/src/app/forgot-username/forgot-username.component.ts
+++ movieBookingApp/src/app/forgot-username/forgot-username.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-username',
   templateUrl: './forgot-username.component.html',
   styleUrls: ['./forgot-username.component.css']
 })
-export class ForgotUsernameComponent implements OnInit{
-
+export class ForgotUsernameComponent implements OnInit {
   forgotUserNameForm!: FormGroup;
   userName!: string;
-  
-  constructor(private authService: AuthService) { }
+  isPopupVisible = false;
+  popupMessage = '';
+  isError = false;
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.userName = '';
@@ -23,41 +26,40 @@ export class ForgotUsernameComponent implements OnInit{
   }
 
   onSubmit(): void {
-    console.log(this.forgotUserNameForm.value);
-    this.getUserName(this.forgotUserNameForm.value);
+    this.getUserName();
   }
 
-  getUserName(data: any){
-    //console.log(data);
-
+  getUserName(): void {
     const email = this.forgotUserNameForm.value.email.trim();
-    console.log("Email -> ", email);
     const password = this.forgotUserNameForm.value.password.trim();
-    console.log("password -> ", password);
-
 
     this.authService.forgotUserName(email, password).subscribe(
       (response: any) => {
-        console.log(response);
-        // handle the response as a string here
-        if(typeof response === 'string'){
-          this.userName = response;
-          console.log("username = ", this.userName);
-          
+        if (response === '') {
+          this.popupMessage = 'No User found, username or password is incorrect.';
+          this.isError = true;
+        } else {
+          this.popupMessage = 'Username: ' + response;
+          this.isError = false;
         }
+        this.isPopupVisible = true;
       },
       (error: any) => {
         console.error('HTTP Error:', error);
-        // handle HTTP error here
+        this.popupMessage = 'An error occurred: ' + error.message;
+        this.isPopupVisible = true;
+        this.isError = true;
       }
     );
-    
-    
-    
-    
-    
   }
 
-
-
+  closePopup(): void {
+    this.isPopupVisible = false;
+    if (!this.isError) {
+      this.router.navigate(['/login']); // Navigate to the login component
+    } else {
+      this.router.navigate(['/login']); // Alternatively, you can customize the error handling and navigate to a different error page if desired
+    }
+  }
+  
 }
